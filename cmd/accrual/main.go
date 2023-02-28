b5f7b07afce220b260551111e23e3acc0716cb84
Леонид Ягупов <lyagupov@yandex.ru>
2023-02-28 14:50:57 +0200

    coded base of the service

--- cmd/accrual/main.go
+++ cmd/accrual/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/AbramovArseniy/Gofermart/internal/accrual/handlers"
+	"github.com/AbramovArseniy/Gofermart/internal/accrual/utils/config"
+	"github.com/AbramovArseniy/Gofermart/internal/accrual/utils/storage"
+)
+
+func main() {
+	var storageMock *storage.Storage
+	config := config.New()
+	handler := handlers.New(storageMock)
+
+	router := chi.NewRouter()
+	router.Mount("/", handler.Route())
+
+	log.Fatal(http.ListenAndServe(config.Address, router))
+}
--- internal/accrual/handlers/handlers.go
+++ internal/accrual/handlers/handlers.go
@@ -1,1 +1,44 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/AbramovArseniy/Gofermart/internal/accrual/utils/storage"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type handler struct {
+	storage *storage.Storage
+}
+
+func New(storage *storage.Storage) handler {
+	return handler{
+		storage: storage,
+	}
+}
+
+func (h handler) Route() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/api/orders/:number", h.number)
+	e.POST("/api/orders", h.orders)
+	e.POST("/api/goods", h.goods)
+
+	return e
+}
+
+func (h handler) number(c echo.Context) error {
+	return c.String(http.StatusOK, "number ok")
+}
+
+func (h handler) orders(c echo.Context) error {
+	return c.String(http.StatusOK, "orders ok")
+}
+
+func (h handler) goods(c echo.Context) error {
+	return c.String(http.StatusOK, "goods ok")
+}
